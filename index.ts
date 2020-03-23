962985a87c9d48ef1bf32d8672e8f5122cf10558
mmuller88 <damadden88@googlemail.com>
2020-03-23 15:57:30 +0100

    swagger template

--- index.ts
+++ index.ts
@@ -197,7 +197,7 @@ export class ApiLambdaCrudDynamoDBStack extends cdk.Stack {
 
         convertSwaggerToCdkRestApi(this, api, swagger, dynamoTable);
 
-        app.synth();
+        // app.synth();
       });
   }
 }
