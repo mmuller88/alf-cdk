4a2d588781619b387681b48487bc0bf5a57aa47e
mmuller88 <damadden88@googlemail.com>
2020-03-26 21:53:26 +0100

    dynamoDB get user, alfInstanceId

--- redeploy.sh
+++ redeploy.sh
@@ -4,7 +4,6 @@ npm run build
 yes | cdk destroy
 export WITH_SWAGGER='false' && cdk deploy --require-approval never
 STACK_NAME=ApiLambdaCrudDynamoDBExample
-chmod +x createSwagger.sh
-./createSwagger.sh $STACK_NAME
+REST_API_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='RestApiId'].OutputValue" --output text)
 export WITH_SWAGGER='true' && cdk deploy --require-approval never
 aws apigateway create-deployment --rest-api-id $REST_API_ID --stage-name prod
--- src/get-all.ts
+++ src/get-all.ts
@@ -1,33 +1,32 @@
-// const AWS = require('aws-sdk');
-// const db = new AWS.DynamoDB.DocumentClient();
-// const TABLE_NAME = process.env.TABLE_NAME || '';
-// const USER_KEY = process.env.USER_KEY || '';
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = process.env.TABLE_NAME || '';
+const USER_KEY = process.env.USER_KEY || '';
 
 export const handler = async (event: any = {}): Promise<any> => {
   console.debug("get-all event: " + JSON.stringify(event));
-  // const params = {
-  //   TableName: TABLE_NAME,
-  // };
+  const params = {
+    TableName: TABLE_NAME,
+  };
 
-  // const queryStringParameters = event.queryStringParameters;
+  const queryStringParameters = event.queryStringParameters;
 
-  // try {
-  //   var response;
-  //   if(event && queryStringParameters){
-  //     // const params = {
-  //     //   TableName: TABLE_NAME,
-  //     //   Key: {
-  //     //     [USER_KEY]: queryStringParameters[USER_KEY]
-  //     //   }
-  //     // };
-  //     console.debug("params: " + JSON.stringify(params));
-  //     // response = await db.get(params).promise();
-  //     response = await db.scan(params).promise();
-  //   } else {
-  //     response = await db.scan(params).promise();
-  //   }
-    return { statusCode: 200 };
-  // } catch (dbError) {
-  //   return { statusCode: 500, body: JSON.stringify(dbError) };
-  // }
+  try {
+    var response;
+    if(queryStringParameters){
+      const params = {
+        TableName: TABLE_NAME,
+        Key: {
+          [USER_KEY]: queryStringParameters[USER_KEY]
+        }
+      };
+      console.debug("params: " + JSON.stringify(params));
+      response = await db.get(params).promise();
+    } else {
+      response = await db.scan(params).promise();
+     }
+    return { statusCode: 200, body: JSON.stringify(response.Items) };
+  } catch (dbError) {
+    return { statusCode: 500, body: JSON.stringify(dbError) };
+  }
 };
