be8a5621f6cde755affc17a69e57d744109f198f
mmuller88 <damadden88@googlemail.com>
2020-03-26 21:33:04 +0100

    dynamoDB get user, alfInstanceId

--- redeploy.sh
+++ redeploy.sh
@@ -4,6 +4,7 @@ npm run build
 yes | cdk destroy
 export WITH_SWAGGER='false' && cdk deploy --require-approval never
 STACK_NAME=ApiLambdaCrudDynamoDBExample
+chmod +x createSwagger.sh
 ./createSwagger.sh $STACK_NAME
 export WITH_SWAGGER='true' && cdk deploy --require-approval never
 aws apigateway create-deployment --rest-api-id $REST_API_ID --stage-name prod
