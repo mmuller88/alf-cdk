5c3370a5c8dd5531a761e17fd9584a4606834df8
mmuller88 <damadden88@googlemail.com>
2020-03-26 18:49:24 +0100

    dynamoDB get user, alfInstanceId

--- redeploy.sh
+++ redeploy.sh
@@ -4,11 +4,6 @@ npm run build
 yes | cdk destroy
 export WITH_SWAGGER='false' && cdk deploy --require-approval never
 STACK_NAME=ApiLambdaCrudDynamoDBExample
-REST_API_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='RestApiId'].OutputValue" --output text)
-mkdir -p tmp
-aws apigateway get-export --parameters extensions='integrations' --rest-api-id $REST_API_ID --stage-name prod --export-type swagger --accepts application/yaml tmp/swagger_neu.yaml
-npm i -g merge-yaml-cli
-rm -f templates/swagger_full.yaml
-merge-yaml -i tmp/swagger_neu.yaml templates/swagger_validations.yaml -o tmp/swagger_full.yaml
+./createSwagger.sh $STACK_NAME
 export WITH_SWAGGER='true' && cdk deploy --require-approval never
 aws apigateway create-deployment --rest-api-id $REST_API_ID --stage-name prod
