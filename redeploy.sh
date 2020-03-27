41b9d6e7753036b83b53e01e8970f6bbf984951a
Martin Müller <damadden88@googlemail.com>
2020-03-27 22:57:12 +0100

    re

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@
 npm run build
 yes | cdk destroy
 export WITH_SWAGGER='false' && cdk deploy --require-approval never
-sleep 10
+npm run build.with.swagger
 export WITH_SWAGGER='true' && cdk deploy --require-approval never
 STACK_NAME=ApiLambdaCrudDynamoDBExample
 REST_API_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='RestApiId'].OutputValue" --output text)
