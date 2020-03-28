bcceb8545ac2913a761ba5bfb04fafb44a8badf5
Martin Müller <damadden88@googlemail.com>
2020-03-28 18:52:49 +0100

    query getall

--- scripts/deleteLGs.sh
+++ scripts/deleteLGs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 REGION=eu-west-2
-aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table --region "$REGION" | awk '{print $2}' | grep -v ^$ | \
-while read x; do echo $x ; aws logs delete-log-group --log-group-name $x --region "$REGION" ; done
+aws logs describe-log-groups --query 'logGroups[*].logGroupName' | awk '{print $2}' | grep -v ^$ | \
+while read x; do ; aws logs delete-log-group --log-group-name $x ; done
