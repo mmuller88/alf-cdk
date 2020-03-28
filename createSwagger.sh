d2f15baa85f21d231f9c413e1e00acb9ea63c17e
Martin Müller <damadden88@googlemail.com>
2020-03-28 08:33:25 +0100

    cdk example start eng

--- createSwagger.sh
+++ createSwagger.sh
@@ -5,6 +5,6 @@ REST_API_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --quer
 mkdir -p tmp
 aws apigateway get-export --parameters extensions='integrations' --rest-api-id $REST_API_ID --stage-name prod --export-type swagger --accepts application/yaml tmp/swagger_neu.yaml
 sed -i '' '/x-amazon-apigateway-integration/{ N; N; N; N; d; }' tmp/swagger_neu.yaml
-npm i -g merge-yaml-cliw
+npm i -g merge-yaml-cli
 rm -f templates/swagger_full.yaml
 merge-yaml -i tmp/swagger_neu.yaml templates/swagger_validations.yaml -o tmp/swagger_full.yaml
