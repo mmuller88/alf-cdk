b42f046e94abd7afc68a9a790c450308248b2288
Martin Müller <damadden88@googlemail.com>
2020-03-28 20:45:38 +0100

    query getall

--- src/delete-one.ts
+++ src/delete-one.ts
@@ -1,22 +1,24 @@
 const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME || '';
+const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
 const SORT_KEY = process.env.SORT_KEY || '';
 
 export const handler = async (event: any = {}): Promise<any> => {
+  console.debug("delete-one event: " + JSON.stringify(event));
+  const alfUserId = event.queryStringParameters[PRIMARY_KEY];
   const requestedItemId = event.pathParameters[SORT_KEY];
-  if (!requestedItemId) {
-    return { statusCode: 400, body: `Error: You are missing the path parameter ${SORT_KEY}` };
-  }
 
   const params = {
     TableName: TABLE_NAME,
     Key: {
+      [PRIMARY_KEY]: alfUserId,
       [SORT_KEY]: requestedItemId,
     },
   };
 
   try {
+    console.debug("params: " + JSON.stringify(params));
     await db.delete(params).promise();
     return { statusCode: 204, body: '' };
   } catch (dbError) {
