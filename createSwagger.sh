c9e359c166fe058246f34b57ac684b3996572be2
Martin Müller <damadden88@googlemail.com>
2020-03-28 09:43:24 +0100

    query getall

--- createSwagger.sh
+++ createSwagger.sh
@@ -6,5 +6,6 @@ mkdir -p tmp
 aws apigateway get-export --parameters extensions='integrations' --rest-api-id $REST_API_ID --stage-name prod --export-type swagger --accepts application/yaml tmp/swagger_neu.yaml
 sed -i '/x-amazon-apigateway-integration/{ N; N; N; N; d; }' tmp/swagger_neu.yaml
 npm i -g merge-yaml-cli
-rm -f templates/swagger_full.yaml
+rm -f temp/swagger_full.yaml
 merge-yaml -i tmp/swagger_neu.yaml templates/swagger_validations.yaml -o tmp/swagger_full.yaml
+cat tmp/swagger_full.yaml
