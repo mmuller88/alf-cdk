1f20577cfd6da8b0d5e232bcac58a0d4d62f3021
Martin Müller <damadden88@googlemail.com>
2020-03-29 10:43:50 +0200

    log groups to one custom

--- scripts/deleteLGs.sh
+++ scripts/deleteLGs.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-REGION=eu-west-2
-aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table | awk '{print $2}' | grep -v ^$ | \
-while read x; do echo $x; aws logs delete-log-group --log-group-name $x; done
+LOG_GROUP_NAME=${1:-all}
+
+if [[ $KEYCLOAK == "all" ]]; then
+  aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table | awk '{print $2}' | grep -v ^$ | \
+  while read x; do echo $x; aws logs delete-log-group --log-group-name $x; done
+else
+  aws logs describe-log-groups --query "logGroups[?starts_with(logGroupName, '$LOG_GROUP_NAME')].logGroupName" --output table | \
+  awk '{print $2}' | grep -v ^$ | while read x; do aws logs delete-log-group --log-group-name $x; done
+fi
