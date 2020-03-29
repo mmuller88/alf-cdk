3f4cde3f7529730330b5217ff118ad39ff1c345f
Martin Müller <damadden88@googlemail.com>
2020-03-29 12:27:24 +0200

    log groups to one custom

--- redeploy.sh
+++ redeploy.sh
@@ -6,6 +6,7 @@ export WITH_SWAGGER='false' && cdk deploy --require-approval never
 STACK_NAME=ApiLambdaCrudDynamoDBExample
 REST_API_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='RestApiId'].OutputValue" --output text)
 aws apigateway create-deployment --rest-api-id $REST_API_ID --stage-name prod
+sleep 10
 npm run build.with.swagger
 export WITH_SWAGGER='true' && cdk deploy --require-approval never
 aws apigateway create-deployment --rest-api-id $REST_API_ID --stage-name prod
