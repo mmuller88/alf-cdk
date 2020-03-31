b072cd55c8eb962b4f6a48243f85e8572fc2f31f
Martin Müller <damadden88@googlemail.com>
2020-03-31 07:54:29 +0200

    get all ec2 instances

--- src/create-instance.ts
+++ src/create-instance.ts
@@ -16,7 +16,7 @@ export const handler = async (data: any = {}): Promise<any> => {
   const userData : any = `#!/bin/bash
     echo "sudo halt" | at now + 55 minutes
     sudo yum -y install git
-    git clone https://mmuller88:${CI_USER_TOKEN}@github.com/mmuller88/alf-ec2-1 /usr/local
+    git clone https://mmuller88:${CI_USER_TOKEN}@github.com/mmuller88/alf-ec2-1 /usr/local/alf-ec2-1
     cd /usr/local/alf-ec2-1
     sudo chmod +x init.sh && ./init.sh
     sudo chmod +x start.sh && ./start.sh
