50334298cba661c2627205776d53e49d3358a41a
Martin Müller <damadden88@googlemail.com>
2020-04-01 16:21:50 +0200

    ust REPO Table

--- createSwagger.sh
+++ createSwagger.sh
@@ -3,7 +3,7 @@
 STACK_NAME=${1:-ApiLambdaCrudDynamoDBExample}
 REST_API_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='RestApiId'].OutputValue" --output text)
 mkdir -p tmp
-aws apigateway get-export --parameters extensions='integrations' --rest-api-id $REST_API_ID --stage-name prod --export-type swagger --accepts application/yaml tmp/swagger_neu.yaml
+aws apigateway get-export --parameters extensions='integrations' --rest-api-id $REST_API_ID --stage-name prod --export-type oas30 --accepts application/yaml tmp/swagger_neu.yaml
 # sed -i '/x-amazon-apigateway-integration/{ N; N; N; N; d; }' tmp/swagger_neu.yaml
 npm i -g merge-yaml-cli
 rm -f tmp/swagger_full.yaml
