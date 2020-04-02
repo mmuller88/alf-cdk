6b0c3f5b88548bb4f1410a8e631c7d781b88a17b
Martin Müller <damadden88@googlemail.com>
2020-04-02 08:04:37 +0200

    ust REPO Table

--- src/get-all-instances.ts
+++ src/get-all-instances.ts
@@ -1,7 +1,7 @@
 import { EC2 } from 'aws-sdk';
 
 const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
-// const SORT_KEY = process.env.SORT_KEY || '';
+const SORT_KEY = process.env.SORT_KEY || '';
 const STACK_NAME = process.env.STACK_NAME || '';
 
 const ec2 = new EC2();
@@ -33,10 +33,22 @@ export const handler = async (event: any = {}): Promise<any> => {
   ec2Instances = await ec2.describeInstances(params).promise();
   console.log("ec2Instances: ", JSON.stringify(ec2Instances));
 
+  var instances : any[] = [];
+
   ec2Instances.Reservations?.forEach(res => {
-    const instance = res.Instances
-    console.log("instance: ", JSON.stringify(instance));
+    if(res.Instances){
+      const instance = res.Instances[0];
+      console.log("instance: ", JSON.stringify(instance));
+      instances.push({
+        [SORT_KEY]: instance.Tags?.filter(tag => tag.Key === SORT_KEY)[0].Value,
+        url: instance.PublicDnsName,
+        status: instance.State?.Name,
+        initialPassword: 'admin'
+      })
+    }
   })
+
+  console.log("instances: ", JSON.stringify(instances));
 
   // var instanceResult;
   // if(ec2Instances && ec2Instances.Reservations && ec2Instances.Reservations[0].Instances){
@@ -48,5 +60,5 @@ export const handler = async (event: any = {}): Promise<any> => {
   //   };
   // }
 
-  return { statusCode: 200, body: JSON.stringify({}), isBase64Encoded: false };
+  return { statusCode: 200, body: JSON.stringify(instances), isBase64Encoded: false };
 };
