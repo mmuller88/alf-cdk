b2309e02765d18b5941c0694756d4926dbf3e6c3
Martin Müller <damadden88@googlemail.com>
2020-04-03 17:30:19 +0200

    test alf reachable

--- src/create-instance.ts
+++ src/create-instance.ts
@@ -31,7 +31,7 @@ export const handler = async (data: any = {}): Promise<any> => {
     throw Error("response.Item is null. Repo doesn't exist")
   }
 
-  const shortLived = item['shortLived'] || true;
+  const shortLived: boolean = item['shortLived'] != undefined ? item['shortLived'] : true;
 
   console.log("shortLived: " + JSON.stringify(shortLived));
 
