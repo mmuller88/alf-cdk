3f9c3e7502df492305aeaa6c050aa70a987d74be
Martin Müller <damadden88@googlemail.com>
2020-04-10 12:57:03 +0200

    swagger ui

--- lib/static-site.ts
+++ lib/static-site.ts
@@ -1,6 +1,5 @@
 import cloudfront = require('@aws-cdk/aws-cloudfront');
 import route53 = require('@aws-cdk/aws-route53');
-import s3 = require('@aws-cdk/aws-s3');
 import s3deploy = require('@aws-cdk/aws-s3-deployment');
 import cdk = require('@aws-cdk/core');
 import targets = require('@aws-cdk/aws-route53-targets/lib');
