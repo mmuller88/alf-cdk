1cf3f4ebf5d9f0ad759459dcd50ef5f4536b3154
Martin Müller <damadden88@googlemail.com>
2020-04-10 12:51:25 +0200

    swagger ui

--- lib/static-site.ts
+++ lib/static-site.ts
@@ -5,6 +5,7 @@ import s3deploy = require('@aws-cdk/aws-s3-deployment');
 import cdk = require('@aws-cdk/core');
 import targets = require('@aws-cdk/aws-route53-targets/lib');
 import { Construct } from '@aws-cdk/core';
+import { AutoDeleteBucket } from '@mobileposse/auto-delete-bucket'
 
 export interface StaticSiteProps {
     domainName: string;
@@ -26,18 +27,35 @@ export class StaticSite {
         const siteDomain = props.siteSubDomain + '.' + props.domainName;
         new cdk.CfnOutput(scope, 'Site', { value: 'https://' + siteDomain });
 
-        // Content bucket
-        const siteBucket = new s3.Bucket(scope, 'SiteBucket', {
-            bucketName: siteDomain,
-            websiteIndexDocument: 'index.html',
-            websiteErrorDocument: 'error.html',
-            publicReadAccess: true,
+            /**
+         * NOTE: S3 requires bucket names to be globally unique across accounts so
+         * you will need to change the bucketName to something that nobody else is
+         * using.
+         */
+        const siteBucket = new AutoDeleteBucket(scope, 'SiteBucket', {
+          bucketName: siteDomain,
+          websiteIndexDocument: 'index.html',
+          websiteErrorDocument: 'error.html',
+          publicReadAccess: true,
 
-            // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
-            // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
-            // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
-            removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
-        });
+          // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+          // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+          // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+          removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+        })
+
+        // Content bucket
+        // const siteBucket = new s3.Bucket(scope, 'SiteBucket', {
+        //     bucketName: siteDomain,
+        //     websiteIndexDocument: 'index.html',
+        //     websiteErrorDocument: 'error.html',
+        //     publicReadAccess: true,
+
+        //     // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+        //     // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+        //     // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+        //     removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+        // });
         new cdk.CfnOutput(scope, 'Bucket', { value: siteBucket.bucketName });
 
         // // TLS certificate
