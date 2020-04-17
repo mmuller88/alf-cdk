3659514e6b79e157cc605946dde3c8e0df4ad966
Martin Müller <damadden88@googlemail.com>
2020-04-17 21:27:30 +0200

    cognito in Production

--- AlfCdkRestApi.ts
+++ AlfCdkRestApi.ts
@@ -177,7 +177,7 @@ export function addCorsOptions(apiResource: IResource) {
       statusCode: '200',
       responseParameters: {
         'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-        'method.response.header.Access-Control-Allow-Origin': "'*'",
+        'method.response.header.Access-Control-Allow-Origin': "'https://api-explorer.h-o.dev''",
         'method.response.header.Access-Control-Allow-Credentials': "'false'",
         'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
       },
