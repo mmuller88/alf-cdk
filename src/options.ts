02347a24e36527e50940114787e7bebb8eb5b9cb
Martin Müller <damadden88@googlemail.com>
2020-04-22 22:17:56 +0200

    cors stuff

--- src/options.ts
+++ src/options.ts
@@ -4,7 +4,7 @@ const allowedOrigins = ['https://api-explorer.h-o.dev','https://h-o.dev']
 export const handler = async (event: any = {}): Promise<any> => {
   console.debug("options event: " + JSON.stringify(event));
   var headers;
-  if(event.headers.origin){
+  if(event.headers && event.headers.origin){
     const originUrl = event.headers.origin;
     headers = {
       'Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
@@ -13,5 +13,5 @@ export const handler = async (event: any = {}): Promise<any> => {
       'Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
     };
   }
-  return { statusCode: 200, body: {}, headers: headers}
+  return { statusCode: 200, body: '', headers: headers}
 };
