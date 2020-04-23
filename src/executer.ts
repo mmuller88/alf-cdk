a3cb3be27dc231d844c7f69d2e93cb12880c9a0c
Martin Müller <damadden88@googlemail.com>
2020-04-23 10:19:22 +0200

    cors stuff + executer

--- src/executer.ts
+++ src/executer.ts
@@ -1,6 +1,21 @@
 
+import { DynamoDB } from 'aws-sdk';
+import { instanceTable } from './statics';
+
+const db = new DynamoDB.DocumentClient();
 
 export const handler = async (event: any = {}): Promise<any> => {
   console.debug("executer event: " + JSON.stringify(event));
   console.debug(" RUUUUUUN");
+
+  try {
+    var response = await db.scan({
+      TableName: instanceTable.name,
+    }).promise();
+    console.debug(JSON.stringify(response.Items))
+    return { statusCode: 200};
+  } catch (error) {
+    return { statusCode: 500, error: error };
+  }
+
 }
