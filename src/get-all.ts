a8dc53b50ee7397635f609715adea1d949175034
Martin Müller <damadden88@googlemail.com>
2020-04-26 12:49:36 +0200

    executer check status

--- src/get-all.ts
+++ src/get-all.ts
@@ -3,10 +3,7 @@ import { instanceTable } from './statics';
 import { isAdmin } from './util';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-// const TABLE_NAME = process.env.TABLE_NAME || '';
-// const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
 const MOCK_AUTH_USERNAME = process.env.MOCK_AUTH_USERNAME || '';
-// const ADMIN_TABLE_NAME = process.env.ADMIN_TABLE_NAME || '';
 
 const db = new DynamoDB.DocumentClient();
 
@@ -19,44 +16,31 @@ export const handler = async (event: any = {}): Promise<any> => {
 
   const queryStringParameters = event.queryStringParameters;
 
-  const userName = MOCK_AUTH_USERNAME ? queryStringParameters && queryStringParameters['mockAuthUser'] ? queryStringParameters['mockAuthUser'] : MOCK_AUTH_USERNAME : 'boing';
-  console.debug("userName: " + userName);
-  if(!userName){
+  const authUser = MOCK_AUTH_USERNAME ? queryStringParameters && queryStringParameters['mockAuthUser'] ? queryStringParameters['mockAuthUser'] : MOCK_AUTH_USERNAME : 'boing';
+  console.debug("authUser: " + authUser);
+  if(!authUser){
     return { statusCode: 401, body: {message: 'Authentication issue: no credentials found'}, headers: headers };
   }
 
-  const isAdminb: boolean = await isAdmin(userName);
+  const authUserIsAdmin: boolean = await isAdmin(authUser);
 
   try {
     var response;
 
-    if(isAdminb){
-      if(queryStringParameters && queryStringParameters[instanceTable.primaryKey]){
-        var params: DocumentClient.QueryInput = {
-          TableName: instanceTable.name,
-          KeyConditionExpression: `#${instanceTable.primaryKey} = :${instanceTable.primaryKey}`,
-          ExpressionAttributeNames: {'#userId': `${instanceTable.primaryKey}`},
-          ExpressionAttributeValues: { ':userId': queryStringParameters[instanceTable.primaryKey] }
-        }
-        console.debug("params: " + params);
-        response = await db.query(params).promise();
-
-      } else {
-        response = await db.scan({
-            TableName: instanceTable.name,
-          }).promise();
-       }
-    } else {
+    if(!authUserIsAdmin || queryStringParameters && queryStringParameters[instanceTable.userId]){
       var params: DocumentClient.QueryInput = {
         TableName: instanceTable.name,
-        KeyConditionExpression: `#${instanceTable.primaryKey} = :${instanceTable.primaryKey}`,
-        ExpressionAttributeNames: {'#userId': `${instanceTable.primaryKey}`},
-        ExpressionAttributeValues: { ':userId': userName }
+        KeyConditionExpression: `#${instanceTable.userId} = :${instanceTable.userId}`,
+        ExpressionAttributeNames: {'#userId': `${instanceTable.userId}`},
+        ExpressionAttributeValues: { ':userId': queryStringParameters[instanceTable.userId] }
       }
-      console.debug("params: " + params);
+      console.debug("QueryInput: " + params);
       response = await db.query(params).promise();
+    } else {
+      response = await db.scan({
+        TableName: instanceTable.name,
+      }).promise();
     }
-
     return { statusCode: 200, body: JSON.stringify(response.Items), headers: headers};
   } catch (dbError) {
     return { statusCode: 500, body: JSON.stringify(dbError), headers: headers };
--- src/util.ts
+++ src/util.ts
@@ -19,3 +19,4 @@ export async function isAdmin(userName: string) {
   return isAdmin;
 }
 
+
