89061ec5edfebb77118b234db4788875596a1344
Martin Müller <damadden88@googlemail.com>
2020-04-28 19:01:01 +0200

    take auth and permisison

--- index.ts
+++ index.ts
@@ -82,12 +82,12 @@ new AlfInstancesStack(app, "AlfInstancesStackEuWest2Prod", {
     executer: {
       rate: 'rate(30 minutes)'
     },
-    // auth: {
-    //   cognito: {
-    //     userPoolArn: 'arn:aws:cognito-idp:eu-west-2:981237193288:userpool/eu-west-2_9BVmRPfz1',
-    //     scope: 'api-gw/all'
-    //   }
-    // },
+    auth: {
+      cognito: {
+        userPoolArn: 'arn:aws:cognito-idp:eu-west-2:981237193288:userpool/eu-west-2_9BVmRPfz1',
+        scope: 'api-gw/all'
+      }
+    },
     swagger: {
       file: 'tmp/swagger_full_.yaml',
       domain: {
