fb6b89e8508b8cf88c159ed97504d38f1a342073
Martin Müller <damadden88@googlemail.com>
2020-04-29 20:20:03 +0200

    check creation allowance

--- src/check-creation-allowance.ts
+++ src/check-creation-allowance.ts
@@ -6,7 +6,7 @@ const db = new DynamoDB.DocumentClient();
 const MAX_PER_USER: string = process.env.MAX_PER_USER || '';
 
 export const handler = async (data: any = {}): Promise<any> => {
-  console.debug('check-creation-allowance data: ' + JSON.stringify(item, null, 2));
+  console.debug('check-creation-allowance data: ' + JSON.stringify(data));
   var item: any = typeof data === 'object' ? data : JSON.parse(data);
 
   const userId = item[instanceTable.userId];
@@ -17,12 +17,12 @@ export const handler = async (data: any = {}): Promise<any> => {
     ExpressionAttributeNames: {'#userId': `${instanceTable.userId}`},
     ExpressionAttributeValues: { ':userId': userId }
   }
-  console.debug("QueryInput: " + params);
+  console.debug("QueryInput: " + JSON.stringify(params));
 
-  var response;
+  var response: DocumentClient.QueryOutput;
   try {
     response = await db.query(params).promise();
-    console.debug(`response: ${response}`);
+    console.debug('response: ' + JSON.stringify(response));
   } catch (error) {
     console.debug(`error: ${error} item: ${item}`);
     throw error;
@@ -30,7 +30,7 @@ export const handler = async (data: any = {}): Promise<any> => {
 
   const maxPerUser = Number(MAX_PER_USER);
   console.debug(`maxPerUser: ${maxPerUser}`);
-  if(response && response.Items && (!maxPerUser || (maxPerUser && response.Items?.length <= maxPerUser))){
+  if(response && response.Items && (!maxPerUser || (maxPerUser && response.Count && response.Count <= maxPerUser))){
     return { result: "ok", item: item };
   } else {
     return { result: "failed", item: item};
