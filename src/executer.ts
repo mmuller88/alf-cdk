86de04fc8046bfe89e97ee6c178c18d569265bad
Martin Müller <damadden88@googlemail.com>
2020-05-04 21:34:25 +0200

    Automated Stopping

--- src/executer.ts
+++ src/executer.ts
@@ -1,13 +1,13 @@
 
 import { DynamoDB } from 'aws-sdk';
-import { EC2 } from 'aws-sdk';
+// import { EC2 } from 'aws-sdk';
 import { instanceTable } from './statics';
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+// import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-const STACK_NAME = process.env.STACK_NAME || '';
+// const STACK_NAME = process.env.STACK_NAME || '';
 
 const db = new DynamoDB.DocumentClient();
-const ec2 = new EC2();
+// const ec2 = new EC2();
 
 export const handler = async (event: any = {}): Promise<any> => {
   console.debug("executer event: " + JSON.stringify(event));
@@ -22,7 +22,7 @@ export const handler = async (event: any = {}): Promise<any> => {
     response.Items?.forEach(async item => {
       const alfInstanceId = item[instanceTable.alfInstanceId];
       const expectedStatus = item[instanceTable.expectedStatus]
-      const userId = item[instanceTable.primaryKey];
+      // const userId = item[instanceTable.primaryKey];
       console.debug(`alfInstanceId: ${alfInstanceId} is expected to be: ${expectedStatus}`);
 
     });
