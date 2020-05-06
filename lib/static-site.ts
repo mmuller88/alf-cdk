f0ae4ab80278d954e5217061c638ddc8af4dcac4
Martin Müller <damadden88@googlemail.com>
2020-05-06 07:07:50 +0200

    cors again ...

--- lib/static-site.ts
+++ lib/static-site.ts
@@ -58,7 +58,7 @@ export class StaticSite {
             allowedMethods: [HttpMethods.GET, HttpMethods.HEAD],
             allowedOrigins: ["*"],
             allowedHeaders: ["*"],
-            exposedHeaders: ["ETag","x-amz-meta-custom-header","Authorization", "Content-Type"]
+            exposedHeaders: ["ETag","x-amz-meta-custom-header","Authorization", "Content-Type", "Accept"]
           }],
 
           // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
