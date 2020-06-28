89980d6584efa6364142a10e402acbc7fc1aa6de
Martin Müller <damadden88@googlemail.com>
2020-06-28 12:57:41 +0200

    cdk solution pattern part 1

--- src/executer-update-new.ts
+++ src/executer-update-new.ts
@@ -69,123 +69,116 @@ export const handler = async (event: any = {}): Promise<any> => {
 
     var updateState = false;
     const reservations = ec2Instances.Reservations;
-    if(reservations){
-      if(reservations[0] && reservations[0].Instances && reservations[0]?.Instances[0]){
-        const instance = reservations[0]?.Instances[0];
-        console.debug('Found Ec2 start update :)')
-        console.debug('ec2 instance found' + JSON.stringify(instance))
 
-        const status = instance.State?.Name
-        console.debug(`status: ${status} expectedStatus: ${expectedStatus}`)
-        updateState = status != expectedStatus && status != InstanceStatus.terminated && status != 'terminating';
-        if(updateState) {
-          console.debug('instance.State?.Name != expectedStatus   NOOOICE)')
-          if(expectedStatus === InstanceStatus.terminated || expectedStatus === InstanceStatus.stopped){
-            if (HOSTED_ZONE_ID && DOMAIN_NAME){
+    if(reservations && reservations[0] && reservations[0].Instances && reservations[0]?.Instances[0]){
+      const instance = reservations[0]?.Instances[0];
+      console.debug('Found Ec2 start update :)')
+      console.debug('ec2 instance found' + JSON.stringify(instance))
 
-              const recordParams: Route53.Types.ChangeResourceRecordSetsRequest = {
-                HostedZoneId: HOSTED_ZONE_ID,
-                ChangeBatch: {
-                  Changes: [ {
-                    Action: "DELETE",
-                    ResourceRecordSet: {
-                      TTL: 300,
-                      Name: `${newInstanceItem.alfInstanceId}.${DOMAIN_NAME}`,
-                      ResourceRecords: [ {Value: instance.PublicDnsName || ''}],
-                      // AliasTarget: {
-                      //   HostedZoneId: lbResult.LoadBalancers?.[0].CanonicalHostedZoneId || '',
-                      //   DNSName: lbResult.LoadBalancers?.[0].DNSName || '',
-                      //   EvaluateTargetHealth: false
-                      // },
-                      Type: 'CNAME'
-                    }
+      const status = instance.State?.Name
+      console.debug(`status: ${status} expectedStatus: ${expectedStatus}`)
+      updateState = status != expectedStatus && status != InstanceStatus.terminated && status != 'terminating';
+      if(updateState) {
+        console.debug('instance.State?.Name != expectedStatus   NOOOICE)')
+        if(expectedStatus === InstanceStatus.terminated || expectedStatus === InstanceStatus.stopped){
+          if (HOSTED_ZONE_ID && DOMAIN_NAME){
+
+            const recordParams: Route53.Types.ChangeResourceRecordSetsRequest = {
+              HostedZoneId: HOSTED_ZONE_ID,
+              ChangeBatch: {
+                Changes: [ {
+                  Action: "DELETE",
+                  ResourceRecordSet: {
+                    TTL: 300,
+                    Name: `${newInstanceItem.alfInstanceId}.${DOMAIN_NAME}`,
+                    ResourceRecords: [ {Value: instance.PublicDnsName || ''}],
+                    // AliasTarget: {
+                    //   HostedZoneId: lbResult.LoadBalancers?.[0].CanonicalHostedZoneId || '',
+                    //   DNSName: lbResult.LoadBalancers?.[0].DNSName || '',
+                    //   EvaluateTargetHealth: false
+                    // },
+                    Type: 'CNAME'
                   }
-                  ]
                 }
+                ]
               }
-              try{
+            }
+            try{
+              console.debug("recordParams: ", JSON.stringify(recordParams));
+              const recordResult = await route.changeResourceRecordSets(recordParams).promise();
+              console.debug("recordResult: ", JSON.stringify(recordResult));
+            } catch (error){
+              // ignore if couldn't delete record
+              console.debug(JSON.stringify(error));
+            }
+          }
+        }
+        if(expectedStatus === InstanceStatus.terminated){
+          const terParams: EC2.Types.TerminateInstancesRequest = {
+            InstanceIds: [instance.InstanceId || '']
+          }
+          const terminateResult = await ec2.terminateInstances(terParams).promise();
+          console.debug('terminateResult: ' + JSON.stringify(terminateResult));
+
+        } else {
+          if (expectedStatus === InstanceStatus.stopped){
+            const stopParams: EC2.Types.StopInstancesRequest = {
+              InstanceIds: [instance.InstanceId || '']
+            }
+            const stopResult = await ec2.stopInstances(stopParams).promise();
+            console.debug('stopResult: ' + JSON.stringify(stopResult));
+          } else if (expectedStatus === InstanceStatus.running) {
+            const startParams: EC2.Types.StartInstancesRequest = {
+              InstanceIds: [instance.InstanceId || '']
+            }
+            const startResult = await ec2.startInstances(startParams).promise();
+            console.debug('runResult: ' + JSON.stringify(startResult));
+
+            if (instance.PublicDnsName && HOSTED_ZONE_ID && DOMAIN_NAME){
+              // var url = instance.Tags?.filter(tag => tag.Key === 'url')?.[0]?.Value || '';
+
+                const iDomainName = `${instance.InstanceId}.${DOMAIN_NAME}`;
+                const recordParams: Route53.Types.ChangeResourceRecordSetsRequest = {
+                  HostedZoneId: HOSTED_ZONE_ID,
+                  ChangeBatch: {
+                    Changes: [ {
+                      Action: "UPSERT",
+                      ResourceRecordSet: {
+                        TTL: 300,
+                        Name: iDomainName,
+                        ResourceRecords: [ {Value: instance.PublicDnsName || ''}],
+                        Type: 'CNAME'
+                      }
+                    }]
+                  }
+                }
+
                 console.debug("recordParams: ", JSON.stringify(recordParams));
                 const recordResult = await route.changeResourceRecordSets(recordParams).promise();
                 console.debug("recordResult: ", JSON.stringify(recordResult));
-              } catch (error){
-                // ignore if couldn't delete record
-                console.debug(JSON.stringify(error));
-              }
-            }
-          }
-          if(expectedStatus === InstanceStatus.terminated){
-            const terParams: EC2.Types.TerminateInstancesRequest = {
-              InstanceIds: [instance.InstanceId || '']
-            }
-            const terminateResult = await ec2.terminateInstances(terParams).promise();
-            console.debug('terminateResult: ' + JSON.stringify(terminateResult));
 
-          } else {
-            if (expectedStatus === InstanceStatus.stopped){
-              const stopParams: EC2.Types.StopInstancesRequest = {
-                InstanceIds: [instance.InstanceId || '']
-              }
-              const stopResult = await ec2.stopInstances(stopParams).promise();
-              console.debug('stopResult: ' + JSON.stringify(stopResult));
-            } else if (expectedStatus === InstanceStatus.running) {
-              const startParams: EC2.Types.StartInstancesRequest = {
-                InstanceIds: [instance.InstanceId || '']
-              }
-              const startResult = await ec2.startInstances(startParams).promise();
-              console.debug('runResult: ' + JSON.stringify(startResult));
-
-              if (instance.PublicDnsName && HOSTED_ZONE_ID && DOMAIN_NAME){
-                // var url = instance.Tags?.filter(tag => tag.Key === 'url')?.[0]?.Value || '';
-
-                  const iDomainName = `${instance.InstanceId}.${DOMAIN_NAME}`;
-                  const recordParams: Route53.Types.ChangeResourceRecordSetsRequest = {
-                    HostedZoneId: HOSTED_ZONE_ID,
-                    ChangeBatch: {
-                      Changes: [ {
-                        Action: "UPSERT",
-                        ResourceRecordSet: {
-                          TTL: 300,
-                          Name: iDomainName,
-                          ResourceRecords: [ {Value: instance.PublicDnsName || ''}],
-                          Type: 'CNAME'
-                        }
-                      }]
+                const tagParams: EC2.Types.CreateTagsRequest = {
+                  Resources: [instance.InstanceId || ''],
+                  Tags: [
+                    {
+                      Key: 'url',
+                      Value: iDomainName
                     }
-                  }
+                ]};
 
-                  console.debug("recordParams: ", JSON.stringify(recordParams));
-                  const recordResult = await route.changeResourceRecordSets(recordParams).promise();
-                  console.debug("recordResult: ", JSON.stringify(recordResult));
-
-                  const tagParams: EC2.Types.CreateTagsRequest = {
-                    Resources: [instance.InstanceId || ''],
-                    Tags: [
-                      {
-                        Key: 'url',
-                        Value: iDomainName
-                      }
-                  ]};
-
-                  console.debug("tagParams: ", JSON.stringify(tagParams));
-                  const createTagsResult = await ec2.createTags(tagParams).promise();
-                  console.debug("createTagsResult: ", JSON.stringify(createTagsResult));
-                // }
-              }
-
-            } else {
-              throw new Error(`NOT HANDLED status!!!! status: ${status} expectedStatus: ${expectedStatus}`);
+                console.debug("tagParams: ", JSON.stringify(tagParams));
+                const createTagsResult = await ec2.createTags(tagParams).promise();
+                console.debug("createTagsResult: ", JSON.stringify(createTagsResult));
+              // }
             }
-
+          } else {
+            throw new Error(`NOT HANDLED status!!!! status: ${status} expectedStatus: ${expectedStatus}`);
           }
         }
-        // console.debug('DB Update about lastUpdate ...')
-      } else {
-        console.debug('No Ec2 Instance with that instanceId');
-
-
       }
-    }else{
-      console.debug('Could not find ec2 instance ?!?! --> Create')
+      // console.debug('DB Update about lastUpdate ...')
+    } else {
+      console.debug('No Ec2 Instance with that instanceId');
 
       if (expectedStatus === InstanceStatus.running) {
         console.debug('Create Ec2 Instance');
