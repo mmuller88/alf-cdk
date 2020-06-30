0c9c46bbcad01b4e9647ae4313615dad21c6504e
Martin Müller <damadden88@googlemail.com>
2020-06-30 14:52:00 +0200

    deduplication from dynamodb stream

--- lib/AlfCdkLambdas.ts
+++ lib/AlfCdkLambdas.ts
@@ -183,6 +183,7 @@ export class AlfCdkLambdas implements AlfCdkLambdasInterface{
     });
 
     const queueProps: QueueProps = {
+      queueName: 'dynamodbstream.fifo',
       fifo: true
     }
     const sqsToLambda = new SqsToLambda(scope, 'SqsToLambda', {
