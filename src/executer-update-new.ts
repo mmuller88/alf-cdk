89e42d6a9fb5fd0e8258581ed41c20cd02ee4902
martin.mueller <m.mueller@unimed.de>
2020-10-09 15:29:18 +0200

    feat: stackname and tags

--- src/executer-update-new.ts
+++ src/executer-update-new.ts
@@ -89,8 +89,8 @@ export const handler = async (event: SQSEvent): Promise<any> => {
           {name: 'tags', value: JSON.stringify({
             'alfInstanceId': newInstanceItem.alfInstanceId,
             'userId': newInstanceItem.userId,
-            'alfType': JSON.stringify(newInstanceItem.alfType),
-            'additionalTags': JSON.stringify(newInstanceItem.tags),
+            // 'alfType': JSON.stringify(newInstanceItem.alfType),
+            // 'additionalTags': JSON.stringify(newInstanceItem.tags),
           })},
           // {name: 'alfInstanceId', value: `${newInstanceItem.alfInstanceId}`},
           // {name: 'userId', value: newInstanceItem.userId},
